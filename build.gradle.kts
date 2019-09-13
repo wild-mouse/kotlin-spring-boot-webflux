79f32881643cbbf601bf110817836636d3c40edd
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-09-13 15:56:15 +0900

    Remove boot-web to enable Netty

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,6 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
--- src/main/kotlin/com/wildmouse/demo/HelloWorldHandler.kt
+++ src/main/kotlin/com/wildmouse/demo/HelloWorldHandler.kt
@@ -7,6 +7,7 @@ import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Mono
 
+@Component
 class HelloWorldHandler {
     fun helloWorld(request: ServerRequest): Mono<ServerResponse> =
             ServerResponse.ok()
